b486ef3796d1b955a2d97fb7bf7954e13d12a4fb
ankita kumari <ankitak.151098@gmail.com>
2022-03-26 01:27:34 +0530

    commit
    Signed-off-by: ankita kumari <ankitak.151098@gmail.com>

---
+++ Untitled-3.js
@@ -0,0 +1,11 @@
+var a= prompt('enter a:');
+var b=prompt('enter b:');
+
+let temp;
+
+temp=a;
+a=b;
+b=temp;
+
+console.log('a= ${a}');
+console.log('b= ${b}');
---
+++ index.js
@@ -0,0 +1,11 @@
+const ps= require("prompt-sync")();
+const prompt=ps;
+var name;
+var age;
+var add;
+name=prompt("your name:");
+age=parseInt(prompt("your age:"));
+add=prompt("your age");
+console.log("personal info=", "my name is", name, "age", age, "add", add);
+
+
