49ea7c21fcb1f017377389b7db031f28a9b0cd75
ankita kumari <ankitak.151098@gmail.com>
2022-03-26 14:22:25 +0530

    version 1
    Signed-off-by: ankita kumari <ankitak.151098@gmail.com>

---
+++ dom1.js
@@ -0,0 +1,37 @@
+//console.log(document.domain);
+//console.log(document.url);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.querySelectorAll);
+
+//var headerTitle= document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent= 'hello';
+//headerTitle.innerText= 'goodbye';
+
+console.log(document.body);
+console.log(document.header);
+console.log(document.header);
+console.log(document.title);
+console.log(document.getElementById('email'));
+console.log(document.doctype);
+console.log(document.domain);
+var header= document.getElementById('mainheader');
+header.style.borderBottom= 'solid 3px #000';
+header.textContent='hello';
+header.innerText='goodbye';
+console.log(header.innerText);
+var secheader= document.getElementById('secondheader');
+secheader.style.color='red';
+
+
+
+
+
+
+
+
+
+
+
