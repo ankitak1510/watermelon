958df6ebf4035f4867462e1f35295ad3c35bd9a2
ankita kumari <ankitak.151098@gmail.com>
2022-03-27 13:42:21 +0530

    queryselector/all
    Signed-off-by: ankita kumari <ankitak.151098@gmail.com>

--- dom2.js
+++ dom2.js
@@ -6,6 +6,19 @@
  //   items[i].style.fontWeight='bold';
 //}
 
-var li= document.getElementsByTagName('li');
-console.log(li[2]);
-li[2].style.backgroundColor='green';
+//var li= document.getElementsByTagName('li');
+//console.log(li[2]);
+//li[2].style.backgroundColor='green';
+
+var lastitem= document.querySelector('.listgroupitem:last-child');
+lastitem.style.color='blue';
+
+var titles=document.querySelectorAll('.sheader');
+console.log(titles);
+titles.textContent='hello';
+
+var odd= document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#f4f4f4';
+}
+
