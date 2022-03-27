4187e67589ae075521ba80f2512be7b9790e5947
ankita kumari <ankitak.151098@gmail.com>
2022-03-27 13:03:43 +0530

    bytag
    Signed-off-by: ankita kumari <ankitak.151098@gmail.com>

--- dom2.js
+++ dom2.js
@@ -1,7 +1,11 @@
-var items= document.getElementsByClassName('listgroupitem');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor='yellow';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+//var items= document.getElementsByClassName('listgroupitem');
+//console.log(items);
+//console.log(items[2]);
+//items[2].style.backgroundColor='yellow';
+//for(var i=0;i<items.length;i++){
+ //   items[i].style.fontWeight='bold';
+//}
+
+var li= document.getElementsByTagName('li');
+console.log(li[2]);
+li[2].style.backgroundColor='green';
